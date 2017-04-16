a658b9e51cea7bfb4e67edc560a6f69bd29f97f5
wangxiping <527409987@qq.com>
2017-04-16 18:38:31 +0800

    build

---
+++ src/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by hasee on 2017/4/16.
+ */
+require('./index.scss')
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hasee on 2017/4/16.
+ */
+const devModule = require('./webpack-config/dev');
+const prodModule = require('./webpack-config/prod');
+
+let finalModule = {};
+
+let ENV = process.env.NODE_ENV;
+
+switch (ENV) {
+    case 'dev':
+        finalModule = devModule;
+        break;
+    case 'prod':
+        finalModule = prodModule;
+        break;
+    default:
+        break;
+}
+
+module.exports = finalModule;

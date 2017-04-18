8fc42bcef0c7975d1221bdfee440efdbb3eb6cbe
wangxiping <527409987@qq.com>
2017-04-18 23:01:03 +0800

    pub-sub

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
  */
 require('./index.scss')
 $(function () {
+    var created_num = 0;
     $('#pc').on('click',function () {
         $('.create').css({
             width:'70%'
@@ -16,13 +17,25 @@ $(function () {
         $('.create').html('')
     })
     $('#add').on('click',function () {
-        var str = '<div class="created_div"></div>'
+        ++created_num
+        var str = '<div class="created_div" id="created_div_'+created_num+'">'+created_num+'</div>'
         $('#add_cut').before(str)
+        var str_right = "";
+        str_right += "<div class=\"contrl\">\n";
+        str_right += "            <div class=\"num\">"+"编号:"+created_num+"<\/div>\n";
+        str_right += "            <div class=\"contrl_style\">\n";
+        str_right += "                <span>background:<\/span>\n";
+        str_right += "                <input type=\"text\">\n";
+        str_right += "            <\/div>\n";
+        str_right += "        <\/div>\n";
+        $('.left_contrl').append(str_right)
+    })
+    $('#cut').on('click',function () {
+        --created_num
+        $('#add_cut').prev().remove()
     })
     $(document).on('click','.created_div', function () {
         console.log(this.style )
-        console.log($(this).css)
-
     })
 
 

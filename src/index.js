e6dfcb5f1faefaa30eaaa6ec35d216a2805c021f
wangxiping <527409987@qq.com>
2017-04-19 23:42:28 +0800

    auto-h5

--- src/index.js
+++ src/index.js
@@ -18,17 +18,18 @@ $(function () {
     })
     $('#add').on('click',function () {
         ++created_num
-        var str = '<div class="created_div" id="created_div_'+created_num+'">'+created_num+'</div>'
+        var str = '<div style=width:100%; class="created_div" id="created_div_'+created_num+'">'+created_num+'</div>'
         $('#add_cut').before(str)
-        var str_right = "";
-        str_right += "<div class=\"contrl\">\n";
-        str_right += "            <div class=\"num\">"+"编号:"+created_num+"<\/div>\n";
-        str_right += "            <div class=\"contrl_style\">\n";
-        str_right += "                <span>background:<\/span>\n";
-        str_right += "                <input type=\"text\">\n";
-        str_right += "            <\/div>\n";
-        str_right += "        <\/div>\n";
-        $('.left_contrl').append(str_right)
+        var strVar = "";
+        strVar += "<div class=\"contrl\">\n";
+        strVar += "            <div class=\"num\">"+created_num+"<\/div>\n";
+        strVar += "            <div class=\"form-group\">\n";
+        strVar += "                <label for=\"exampleInputName"+created_num+"\">设置背景图片:<\/label>\n";
+        strVar += "                <input type=\"text\" class=\"form-control\" id=\"exampleInputName"+created_num+"\" placeholder=\"\">\n";
+        strVar += "            <\/div>\n";
+        strVar += "            <button type=\"button\" class=\"btn btn-primary\" data-id=\"created_div_"+created_num+"\">确认<\/button>\n";
+        strVar += "        <\/div>\n";
+        $('.left_contrl').append(strVar)
     })
     $('#cut').on('click',function () {
         --created_num
@@ -36,6 +37,16 @@ $(function () {
     })
     $(document).on('click','.created_div', function () {
         console.log(this.style )
+    })
+    $(document).on('keyup','.form-control',function () {
+        $(this).parent().next().attr('data-background',$(this).val())
+        console.log($(this).parent().next())
+    })
+    $(document).on('click','.btn',function () {
+        //alert(this)
+        var id =$(this).attr('data-id')
+        var background=$(this).attr('data-background')
+        $('#'+id).append( "        <img src=\""+background+"\" style=\"display: block; width: 100%\" alt=\"\">\n")
     })
 
 
